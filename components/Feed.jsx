819c89d74fe2f2e1a51d551d5a9e52d4b9c856c1
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-02 21:51:26 +0530

    Add Search Feature

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,13 +18,13 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  // const [post, setAllPosts] = useState([]);
+
   const [post, setPosts] = useState([]);
 
   // // Search states
   const [searchText, setSearchText] = useState("");
-  // const [searchTimeout, setSearchTimeout] = useState(null);
-  // const [searchedResults, setSearchedResults] = useState([]);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt");
@@ -37,35 +37,35 @@ const Feed = () => {
     fetchPosts();
   }, []);
 
-  // const filterPrompts = (searchtext) => {
-  //   const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
-  //   return allPosts.filter(
-  //     (item) =>
-  //       regex.test(item.creator.username) ||
-  //       regex.test(item.tag) ||
-  //       regex.test(item.prompt)
-  //   );
-  // };
-
-  const handleSearchChange = (e) => {
-    // clearTimeout(searchTimeout);
-    // setSearchText(e.target.value);
-
-    // // debounce method
-    // setSearchTimeout(
-    //   setTimeout(() => {
-    //     const searchResult = filterPrompts(e.target.value);
-    //     setSearchedResults(searchResult);
-    //   }, 500)
-    // );
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i");
+    return post.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
   };
 
-  // const handleTagClick = (tagName) => {
-  //   setSearchText(tagName);
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
 
-  //   const searchResult = filterPrompts(tagName);
-  //   setSearchedResults(searchResult);
-  // };
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className='feed'>
@@ -82,16 +82,14 @@ const Feed = () => {
       </form>
 
       {/* All Prompts */}
-      {/* {searchText ? ( */}
+      {searchText ? (
         <PromptCardList
-          data={post}
-          handleTagClick={() => {}}
-          // data={searchedResults}
-          // handleTagClick={handleTagClick}
+          data={searchedResults}
+          handleTagClick={handleTagClick}
         />
-      {/* ) : (
-        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
-      )} */}
+      ) : (
+        <PromptCardList data={post} handleTagClick={handleTagClick} />
+      )} 
     </section>
   );
 };
