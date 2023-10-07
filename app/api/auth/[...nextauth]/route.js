ade11d9d49ff268043feb443824eef95b76917b5
Priyanshu085 <abpriyanshu007@gmail.com>
2023-10-07 22:58:58 +0530

    Fixing SignIn Issue

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,17 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-
 import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
+  
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+
   callbacks: {
     async session({ session }) {
       // store the user id from MongoDB to session
@@ -19,13 +20,12 @@ const handler = NextAuth({
 
       return session;
     },
+    
     async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
-
         // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
-
         // if not, create a new document and save user in MongoDB
         if (!userExists) {
           await User.create({
@@ -34,14 +34,13 @@ const handler = NextAuth({
             image: profile.picture,
           });
         }
-
         return true
       } catch (error) {
         console.log("Error checking if user exists: ", error.message);
         return false
       }
-    },
+    }
   }
 })
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
