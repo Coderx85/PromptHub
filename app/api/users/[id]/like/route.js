8389a118cd4bc16452ae7078e3368c96413f477b
Priyanshu085 <abpriyanshu007@gmail.com>
2024-06-01 15:04:46 +0530

    chore: Refactor like functionality in PromptCard component

--- app/api/users/[id]/like/route.js
+++ app/api/users/[id]/like/route.js
@@ -1,29 +1,29 @@
-import { connectToDB } from '@utils/database';
-import Prompt from '@models/prompt';
+// import { connectToDB } from '@utils/database';
+// import Prompt from '@models/prompt';
 
-export default async function handler(req, res) {
-  const { method } = req;
-  const { id } = req.query;
+// export default async function handler(req, res) {
+//   const { method } = req;
+//   const { id } = req.query;
 
-  await connectToDB();
+//   await connectToDB();
 
-  switch (method) {
-    case 'PATCH':
-      try {
-        const prompt = await Prompt.findById(id);
-        if (!prompt) return res.status(404).json({ error: 'Prompt not found' });
+//   switch (method) {
+//     case 'PATCH':
+//       try {
+//         const prompt = await Prompt.findById(id);
+//         if (!prompt) return res.status(404).json({ error: 'Prompt not found' });
 
-        prompt.likes += 1;
-        await prompt.save();
+//         prompt.likes += 1;
+//         await prompt.save();
 
-        res.status(200).json(prompt);
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-      break;
+//         res.status(200).json(prompt);
+//       } catch (error) {
+//         res.status(500).json({ error: error.message });
+//       }
+//       break;
 
-    default:
-      res.setHeader('Allow', ['PATCH']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
-}
+//     default:
+//       res.setHeader('Allow', ['PATCH']);
+//       res.status(405).end(`Method ${method} Not Allowed`);
+//   }
+// }
--- app/api/users/[id]/likes/route.js
+++ app/api/users/[id]/likes/route.js
@@ -1,28 +1,28 @@
-// pages/api/users/[id]/likes.js
+// // pages/api/users/[id]/likes.js
 
-import { connectToDB } from '@utils/database';
-import User from '@models/user';
+// import { connectToDB } from '@utils/database';
+// import User from '@models/user';
 
-export default async function handler(req, res) {
-  const { method } = req;
-  const { id } = req.query;
+// export default async function handler(req, res) {
+//   const { method } = req;
+//   const { id } = req.query;
 
-  await connectToDB();
+//   await connectToDB();
 
-  switch (method) {
-    case 'GET':
-      try {
-        const user = await User.findById(id).populate('likes');
-        if (!user) return res.status(404).json({ error: 'User not found' });
+//   switch (method) {
+//     case 'GET':
+//       try {
+//         const user = await User.findById(id).populate('likes');
+//         if (!user) return res.status(404).json({ error: 'User not found' });
 
-        res.status(200).json(user.likes);
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-      break;
+//         res.status(200).json(user.likes);
+//       } catch (error) {
+//         res.status(500).json({ error: error.message });
+//       }
+//       break;
 
-    default:
-      res.setHeader('Allow', ['GET']);
-      res.status(405).end(`Method ${method} Not Allowed`);
-  }
-}
+//     default:
+//       res.setHeader('Allow', ['GET']);
+//       res.status(405).end(`Method ${method} Not Allowed`);
+//   }
+// }
